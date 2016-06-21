75cd08f5d78ba429f94e45062103959724002c75
CJ Willis <cjwillis809@gmail.com>
2016-06-21 14:46:50 -0500

    Initial commit with layout

---
+++ app/src/main/java/com/cj/randomstreet/StreetHomeActivity.java
@@ -0,0 +1,13 @@
+package com.cj.randomstreet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StreetHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_street_home);
+    }
+}
